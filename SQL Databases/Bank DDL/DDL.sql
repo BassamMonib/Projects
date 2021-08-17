90ba49984885c28cb311c87db5677d5994a0c098
BassamMonib <bassam.munib@gmail.com>
2021-08-17 20:39:02 +0500

    Projects

--- SQL Databases/Bank DDL/DDL.sql
+++ SQL Databases/Bank DDL/DDL.sql
@@ -2,7 +2,7 @@ show databases;
 use bankschema;
 
 create table employee(
-	eid varchar(10) PRIMARY KEY,
+    eid varchar(10) PRIMARY KEY,
     branch_id varchar(15),
     name varchar(25),
     address varchar(50),
@@ -20,3 +20,8 @@ alter table loanaccounts alter max_credit set default 1000;
 alter table employee modify age int;
 
 alter table transaction add constraint FOREIGN KEY (eid) REFERENCES employee (eid) on delete cascade on update cascade;
+
+insert into employee (eid, branch_id, name, address, age) values 
+(1, 32, 'Bob', '1318 Walt Nuzum Farm Road', 21),
+(2, 43, 'Alice', '4479 Commerce Boulevard', 25),
+(3, 56, 'Joe', '1873 Bridge Avenue', 39);
